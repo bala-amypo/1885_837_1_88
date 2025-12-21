e23dbef66e3a0c76c77a838f8c665abf6a8eb708
bala-amypo <bala@amypo.in>
2025-12-21 07:02:54 +0000

    User commit

--- src/main/java/com/example/demo/model/BookingLog.java
+++ src/main/java/com/example/demo/model/BookingLog.java
@@ -1,53 +1,28 @@
 package com.example.demo.model;
 
+import jakarta.persistence.*;
+import lombok.*;
 import java.time.LocalDateTime;
 
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class BookingLog {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne
     private Booking booking;
+
     private String logMessage;
+
     private LocalDateTime loggedAt;
 
-    public BookingLog() {
-    }
-
-    public BookingLog(Long id, Booking booking, String logMessage, LocalDateTime loggedAt) {
-        this.id = id;
-        this.booking = booking;
-        this.logMessage = logMessage;
-        this.loggedAt = loggedAt;
-    }
-
-    public Long getId() {
-        return id;
-    }
-    
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Booking getBooking() {
-        return booking;
-    }
-    
-    public void setBooking(Booking booking) {
-        this.booking = booking;
-    }
-
-    public String getLogMessage() {
-        return logMessage;
-    }
-    
-    public void setLogMessage(String logMessage) {
-        this.logMessage = logMessage;
-    }
-
-    public LocalDateTime getLoggedAt() {
-        return loggedAt;
-    }
-    
-    public void setLoggedAt(LocalDateTime loggedAt) {
-        this.loggedAt = loggedAt;
+    @PrePersist
+    public void onCreate() {
+        loggedAt = LocalDateTime.now();
     }
 }
