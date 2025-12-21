95c87661f831aab0132c839de5c40873cb76cf14
bala-amypo <bala@amypo.in>
2025-12-21 16:40:30 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,22 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class DemoApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
-
-    // Add this bean so Spring can inject PasswordEncoder
-   @Bean
-public PasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder();
 }
-
-    }
-
--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -7,6 +7,7 @@ import lombok.*;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "users")
 public class User {
 
     @Id
@@ -20,5 +21,5 @@ public class User {
 
     private String password;
 
-    private String role; // RESIDENT / ADMIN
+    private String role = "RESIDENT";
 }
