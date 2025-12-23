3d9388134d7b5d757fc38b6ee9d5a5fe2c02035c
bala-amypo <bala@amypo.in>
2025-12-23 05:07:42 +0000

    User commit

--- src/main/java/com/example/demo/controller/FacilityController.java
+++ src/main/java/com/example/demo/controller/FacilityController.java
@@ -4,6 +4,7 @@ import com.example.demo.model.Facility;
 import com.example.demo.service.FacilityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/facilities")
