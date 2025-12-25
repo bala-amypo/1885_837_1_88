43d310ceb949b6ea8b3b6bd4e4e72bffc9f9950e
bala-amypo <bala@amypo.in>
2025-12-25 14:47:26 +0000

    User commit

--- src/main/java/com/example/demo/repository/BookingLogRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.model.BookingLog;
-import java.util.List;
-
-public interface BookingLogRepository extends JpaRepository<BookingLog, Long> {
-    List<BookingLog> findAllByBookingId(Long bookingId);
-}
--- src/main/java/com/example/demo/service/ApartmentUnitService.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.model.ApartmentUnit;
-
-public interface ApartmentUnitService {
-    ApartmentUnit assignUnitToUser(Long userId, ApartmentUnit unit);
-    ApartmentUnit getUnitByUser(Long userId);
-}
