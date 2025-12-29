7715ecd2f2ce75ae8e19c372a5fdd7ea616031da
bala-amypo <bala@amypo.in>
2025-12-29 05:04:57 +0000

    User commit

--- src/main/java/com/example/demo/service/ApartmentUnitService.java
+++ src/main/java/com/example/demo/service/ApartmentUnitService.java
@@ -1,10 +1,17 @@
 package com.example.demo.service;
 
 import com.example.demo.model.ApartmentUnit;
+import java.util.List;
 
 public interface ApartmentUnitService {
 
-    ApartmentUnit assignUnitToUser(Long userId, ApartmentUnit unit);
+    ApartmentUnit createApartmentUnit(ApartmentUnit unit);
 
-`    ApartmentUnit getUnitByUser(Long userId);
+    ApartmentUnit getApartmentUnit(Long id);
+
+    List<ApartmentUnit> getAllApartmentUnits();
+
+    ApartmentUnit updateApartmentUnit(Long id, ApartmentUnit unit);
+
+    void deleteApartmentUnit(Long id);
 }
